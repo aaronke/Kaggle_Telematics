9a265cb4d5e4044d8e0163a0a4a390cc94c224cc
zkfairytale <kai.zhou2013@gmail.com>
2015-03-14 22:41:12 -0600

    1,pure 305 is better than 171, 0.9069; 2, run 6 experiment,  ada_rf, ada_extra, pure_extra for 171 and 305

--- RF/RF_train_171.py
+++ RF/RF_train_171.py
@@ -2,12 +2,14 @@ import os
 import time
 from sklearn.ensemble import GradientBoostingClassifier as GBRT
 from sklearn.ensemble import RandomForestClassifier as RandomForest
+from sklearn.ensemble import ExtraTreesClassifier as ExtraTrees
+from sklearn.ensemble import AdaBoostClassifier as AdaBoost
 from sklearn.linear_model import LogisticRegression as LR
 from sklearn.preprocessing import Imputer
 import numpy as np
 import cPickle as pickle
 
-featureToUse = 'feature134'
+featureToUse = 'feature171'
 path = '/cshome/kzhou3/Data/feature/' + featureToUse + '/'
 with open(path + featureToUse + '_1','rb') as fp:
     f1 = np.array(pickle.load(fp))
@@ -18,11 +20,13 @@ with open(path + featureToUse + '_3','rb') as fp:
 with open(path + featureToUse + '_4','rb') as fp:
     f4 = np.array(pickle.load(fp))
 feature = np.concatenate((f1,f2,f3,f4))
-#feature = feature[:,24:] # 147 sub_feature
+
 with open(path + 'name','rb') as fp:
     name = pickle.load(fp)
 
-output = open('/cshome/kzhou3/Dropbox/Telematics/submission/RF171/134_new_RF1888auto.csv', 'w')
+#output = open('/cshome/kzhou3/Dropbox/Telematics/submission/final/171_ada_RF.csv', 'w')
+#output = open('/cshome/kzhou3/Dropbox/Telematics/submission/final/171_ada_ET.csv', 'w')
+output = open('/cshome/kzhou3/Dropbox/Telematics/submission/final/171_ET.csv', 'w')
 output.write('driver_trip,prob\n')
 
 start = time.time()
@@ -35,11 +39,11 @@ for k in range(1,size + 1):
         X = np.concatenate((X, feature[(k -1 + i)%size*200:((k -1 + i)%size*200 + 1)]))
     X = Imputer().fit_transform(X)
     y = np.array([0]*200 + [1]*neg)
-    #clf = RandomForest(n_estimators=550, max_features=8, max_depth=None, min_samples_split=1) # 0.90080
-    clf = RandomForest(n_estimators=1888, max_depth=None, min_samples_split=1) # 0.90464
-    #clf = RandomForest(n_estimators=1888, max_features=8, max_depth=None, min_samples_split=1) # 0.90369
-    #clf = GBRT(n_estimators=350, learning_rate=0.08, max_depth=6, max_features="auto", min_samples_leaf=5, random_state=0, subsample = 0.5)
-    #clf = LR(penalty='l2', dual=False, C=0.8, fit_intercept=True, intercept_scaling=1, random_state=None) # 0.80534
+    #clf_base = RandomForest(n_estimators=1888, max_depth=None, min_samples_split=1) # 0.90464
+    #clf_base = ExtraTrees(n_estimators=1888, max_depth=None, min_samples_split=1) # 0.90464
+    #clf = AdaBoost(clf_base, n_estimators=55, learning_rate=0.8)
+
+    clf = ExtraTrees(n_estimators=1888, max_depth=None, min_samples_split=1) # 0.90464
     clf.fit(X, y)
     # ======== Predict ===============
     X = feature[200*(k-1):200*(k)]
